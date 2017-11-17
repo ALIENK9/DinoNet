0666158d56c8dd8f0487cc851821ba7e4a153677
ALIENK9 <alezanga@hotmail.it>
2017-11-17 17:47:33 +0100

    Aggiunta bozza di menu

---
+++ Sito/menu.php
@@ -0,0 +1,22 @@
+	<div id="menu" class="w3-sidebar w3-bar-block w3-small w3-hide-small w3-center">
+		  <a href="home.html" class="w3-bar-item w3-button w3-padding-large w3-black">
+		  	<i class="fa fa-home w3-xxlarge"></i>
+		    <p>Home</p>
+		  </a>
+		   <a href="storia.html" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
+		    <i class="fa fa-history w3-xxlarge"></i>
+		    <p>Storia</p>
+		  </a>
+		  <a href="specie.html" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
+		    <i class="fa fa-user w3-xxlarge"></i>
+		    <p>Le specie</p>
+		  </a>
+		  <a href="articoli.html" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
+		    <i class="fa fa-eye w3-xxlarge"></i>
+		    <p>Articoli</p>
+		  </a>
+		  <a href="registrazione.html" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
+		    <i class="fa fa-male w3-xxlarge"></i>
+		    <p>Registrazione</p>
+		  </a>
+	</div>
