67cc15bfaaf1330d7652bd7dc6b6772f6a4bfb47
Matte <matteo_rizzo96@hotmail.it>
2017-11-17 17:28:07 +0100

    Create loading.php

---
+++ Sito/loading.php
@@ -0,0 +1,19 @@
+<!-- Loading -->
+
+<div class="loading w3-flat-green-sea">
+	<div class="w3-display-middle w3-center">
+		<img class="bounce" width="300px" src="img/dino-loading.png" style="filter: invert(100%)"/>
+    </div>
+</div>
+
+<!-- Loading - script -->
+<script>
+	$(document).ready(function() {
+		$(window).load(function() {
+		$(".loading").fadeOut("slow");
+		});
+	});
+</script>
+<!-- Loading - script -->
+
+<!-- /Loading -->
