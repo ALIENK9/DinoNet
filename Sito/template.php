117593059b41bce1501e5f9f252f6c9e9eb05fd9
Matte <matteo_rizzo96@hotmail.it>
2017-11-17 17:02:27 +0100

    Create template.php

---
+++ Sito/template.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TecWeb</title>
+	<meta name="description" content="Descrizione">
+	<meta name="author" content="Alessandro Zingari, Cristiano Tessarolo, Matteo Rizzo">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="HTML, CSS, XML, JavaScript">
+	<link rel="stylesheet" href="css/w3.css">
+	<link rel="stylesheet" href="css/index.css">
+	<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-flat.css">
+</head>
+
+<!-- Body -->
+
+<body>
+
+<?php include_once('menu.php') ?>
+
+<!-- 
+	...
+	contenuto
+	...
+-->
+
+<?php include_once('footer.php') ?>
+
+</body>
+
+<!-- /Body -->
+
+</html>
