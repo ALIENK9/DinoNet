a076422049f242b18beaa0c61ae455634a99b596
Matte <matteo_rizzo96@hotmail.it>
2017-11-17 17:02:07 +0100

    Create footer.php

---
+++ Sito/footer.php
@@ -0,0 +1,6 @@
+<footer class="w3-flat-green-sea padding-3">
+	<div class="w3-content w3-center">
+		<h3 class="w3-wide"> <strong> DINOSWAG <em> S.P.A </em> </strong> </h3>
+		<a href="mailto:info@dinoSWAG.com"><p>info@dinoSWAG.com</p></a>
+	</div>
+</footer>
