c67a0625b388cdc0241475a461bd35ba302c5465
Matte <matteo_rizzo96@hotmail.it>
2017-11-17 17:52:45 +0100

    file contenente il bottone e relativo script

---
+++ Sito/tothetop.php
@@ -0,0 +1,58 @@
+<!-- To the top btn -->
+
+<button onclick="topFunction()" id="toTheTop" title="Torna su" class="w3-btn w3-flat-green-sea w3-card">Top</button>
+
+<script>
+// When the user scrolls down 200px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        document.getElementById("toTheTop").style.display = "block";
+    } else {
+        document.getElementById("toTheTop").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+</script>
+
+ 
+<!-- Smooth scrolling -->
+
+<script>
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
+</script>
+
+<!-- / Smooth scrolling -->
+
+
+<!-- /To the top btn -->
