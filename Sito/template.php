35981c9d37bb00cbf6dbd653ff20e6d82366a7f7
Matte <matteo_rizzo96@hotmail.it>
2017-11-17 17:27:48 +0100

    Update template.php

--- Sito/template.php
+++ Sito/template.php
@@ -10,11 +10,14 @@
 	<link rel="stylesheet" href="css/w3.css">
 	<link rel="stylesheet" href="css/index.css">
 	<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-flat.css">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 </head>
 
 <!-- Body -->
 
 <body>
+
+<?php include_once('loading.php') ?>
 
 <?php include_once('menu.php') ?>
 
