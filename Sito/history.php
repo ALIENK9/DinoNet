332d9f265ea3ac59e088fab3d5d8ae26506fce0b
Matte <matteo_rizzo96@hotmail.it>
2017-11-19 19:08:05 +0100

    Create history.php

---
+++ Sito/history.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html>
+<head xml:lang="it-IT" lang="it-IT">
+	<title>La storia dei dinosauri</title>
+	<meta name="description" content="La storia dei dinosauri, spiegata semplicemente">
+	<meta name="author" content="Alessandro Zangari, Cristiano Tessarolo, Matteo Rizzo">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="HTML, CSS, XML, JavaScript">
+	<link rel="stylesheet" href="css/w3.css">
+	<link rel="stylesheet" href="css/index.css">
+	<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-flat.css">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+</head>
+
+<!-- Body -->
+
+<body>
+
+<?php include_once('loading.php') ?>
+
+<?php include_once('menu.php') ?>
+
+<div id="main" class="main w3-card">
+
+<!-- Header -->
+
+<header class="parallax padding-6">
+	<div id="title-card" class="w3-content w3-card w3-center">
+		<h1 class="w3-wide title"> La storia dei dinosauri </h1>
+		<br>
+		<h4>scopri l'affascinante storia di creature vissute milioni di anni fa</h4>
+	</div>
+</header>
+
+<!-- /Header -->
+
+<!-- Content -->
+
+<!-- Chi sono i dinosauri -->
+
+<div class="w3-flat-green-sea padding-6">
+	<div class="w3-content w3-padding-large">
+		<h1 class="w3-text-white"> Chi sono i dinosauri? </h1>
+		<hr>
+		<p>
+			La parola dinosauro deriva dal greco <em lang="el" xml:lang="el">deinos</em>, <strong>terribile</strong>, e <em lang="el" xml:lang="el">sauros</em>,
+			<strong>lucertola</strong>, e venne utilizzata per la prima volta da <strong>Richard Owen</strong>, un biologo e paleontologo britannico, <strong>nel 1842</strong>.
+			
+			<br><br>
+			
+			I dinosauri sono un gruppo di <strong>rettili</strong>, appartenenti alla sottoclasse degli arcosauri, che <strong>vissero sulla terra 200 milioni
+			di anni fa</strong> (durante l'era del <em>Triassico</em>), per circa 160 milioni di anni, con un momento di <strong>massimo splendore durante il 
+			periodo successivo, detto Giurassico</strong>. Sparirono dalla faccia del nostro pianeta circa <strong>65 milioni di anni fa</strong>, alla fine del
+			periodo Cretaceo. Oggi sopravvivono grazie a resti fossili studiati dai paleontologi.
+		</p>
+	</div>
+</div>
+
+<!-- La grande scoperta -->
+
+<div class="w3-row parallax" style="background-image:url('img/dino-1.jpg')">
+	<div class="w3-half padding-12"></div>
+	<div class="w3-half">
+		<div class="w3-white wrap-padding">
+			<h1 class="w3-center"> La grande scoperta e <br> la corsa ai fossili</h1>
+			<br>
+			<p class="w3-container">
+				<strong>All'inizio del secolo scorso</strong>, con il francese <strong>Georges Cuvier</strong> (1769-1832), nasceva la <strong>paleontologia</strong>, 
+				la scienza che studia le forme di vita più antiche basandosi sui ritrovamenti fossili, e cominciava a delinearsi 
+				davanti agli occhi dei ricercatori la figura di uno tra i più straordinari protagonisti della storia del vertebrato, 
+				il dinosauro.
+			</p>
+				
+			<div class="w3-flat-green-sea w3-panel margin-3">
+				<p>
+					La <em>"febbre della caccia al dinosauro"</em> scoppiò nella <strong>seconda metà dell'Ottocento</strong>,
+					quando tutti i musei di storia naturale erano particolarmente desiderosi di possedere degli scheletri
+					di dinosauri. Si organizzarono così svariate spedizioni per individuare i possibili giacimenti di fossili. Nacque una
+					gara affannosa a chi rintracciava il dinosauro più strano, più grande, più terribile. 
+				</p>
+			</div>
+			
+			<p class="w3-container">	
+				In questa competizione si distinsero soprattutto gli americani <strong>Edward Drinker Cope</strong> (1840-1897) e
+				<strong>Othniel Charles Marsh</strong> (1831-1899), dapprima amici, poi accanitissimi rivali. Dei loro emissari, 
+				nel più stretto segreto, percorrevano instancabilmente gli Stati Uniti alla ricerca di un indizio che lasciasse
+				supporre la presenza di fossili. Per evitare che il rivale venisse a conoscenza del luogo di scavo, si partiva in
+				direzioni sbagliate, e solo dopo aver fatto perdere le proprie tracce si decideva di puntare sul luogo prescelto. 
+				<strong>Gli scavi erano frenetici</strong>, si dovevano estrarre tutti i fossili presenti prima che l'avversario venisse a conoscenza
+				della scoperta; ma, come se non bastasse, terminati i lavori, si distruggeva quello che restava, nel timore che il rivale riprendesse le ricerche in quegli
+				stessi luoghi e trovasse ancora qualche fossile importante. La folle gara ebbe almeno come risultato <strong>la 
+				scoperta</strong>, in pochi decenni, <strong>di numerose specie di dinosauri</strong>, poi ricostruiti in laboratorio.
+			</p>
+		</div>
+	</div>
+</div>
+
+<!-- Perché i dinosauri sono unici? -->
+
+<div class="w3-flat-green-sea padding-6">
+	<div class="w3-content w3-padding-large">
+		<h1 class="w3-text-white"> Perché i dinosauri sono unici? </h1>
+		<hr>
+		<p>
+			I dinosauri si differenziano da qualsiasi altro rettile per un aspetto fondamentale: gli arti sono collocati in 
+			maniera perpendicolare al corpo (in modo molto simile agli uccelli o ai mammiferi) e per questo, quando l'animale 
+			è fermo, il suo ventre e la sua coda non toccano il terreno, cosa che non avviene in nessun altro rettile. Inoltre 
+			erano tutti creature terrestri, non volavano e non vivevano nei mari. 
+			
+			<br><br>
+			
+			Erano tuttavia erano capaci di attraversare piccoli specchi di acqua e di nuotare. I rettili volanti, come gli 
+			Pterosauri e i rettili marini, come i Plesiosauri, in realtà erano dei cugini dei dinosauri , così come lo sono, 
+			con una lunghissima parentela, i coccodrilli e le tartarughe .
+		</p>
+	</div>
+</div>
+
+<!-- Bipedi e quadrupedi -->
+
+<div class="w3-row parallax" style="background-image:url('img/dino-3.jpg')">
+	<div class="w3-half">
+		<div class="w3-white wrap-padding">
+			<h1 class="w3-center"> Bipedi e quadrupedi, <br> erbivori e carnivori </h1>
+			<br>
+			<p class="w3-container">
+				I dinosauri si suddividono in <strong>bipedi e quadrupedi</strong>, in <strong>erbivori</strong> (ossia quelli che si
+				nutrono di vegetali) e <strong>carnivori</strong> (ossia quelli che mangiano carne). Gli erbivori erano caratterizzati
+				da un <strong>collo molto lungo</strong>, necessario per arrivare alle foglie degli alberi più alti. I carnivori erano
+				predatori dai <strong>denti affilati</strong> ed estremamente aggressivi.
+				La loro aggressività doveva far paura anche per il fatto che <strong>erano giganteschi</strong> e non smettevano mai
+				di crescere (anche se in età adulta il processo si rallentava)!
+			</p>	
+			
+			<div class="w3-flat-green-sea w3-panel margin-3">
+				<p>
+					Il <strong>brontosauro</strong>, vissuto nel Giurassico, poteva essere lungo più di venti metri e pesare circa 
+					quaranta tonnellate (ossia l'equivalente del peso di 600 uomini!). 
+				</p>
+			</div>
+			
+			<p class="w3-container">	
+				Il <strong>brontosauro</strong> aveva un collo lunghissimo per poter nutrirsi delle foglie più tenere che crescevano in cima agli alberi
+				Ancora più mastodontico era l'<strong>ultrasauro</strong>: alto 16 metri (come una palazzina di 5 piani), lungo 30 (come tre autobus messi in fila) e pesante ben 130 tonnellate (di più di 100 auto!) .
+				Anche il <strong>tirannosauro</strong> (T-rex), il più grande carnivoro estinto, non era da meno: con i suoi 15 metri di lunghezza e il suo peso di 10
+				tonnellate riusciva comunque a camminare in posizione eretta reggendosi sulle zampe posteriori.
+			</p>
+		</div>
+	</div>
+	<div class="w3-half padding-12"></div>
+</div>
+
+<!-- L'estensione -->
+
+<div class="w3-flat-green-sea padding-6">
+	<div class="w3-content w3-padding-large">
+		<h1 class="w3-text-white"> L'estinzione </h1>
+		<hr>
+		<p>
+			<strong>L'estinzione dei dinosauri</strong> è un mistero che da sempre interessa <strong>paleontologi</strong> (coloro che studiano gli animali vissuti
+			sulla terra prima degli attuali), studiosi e semplici appassionati. Al riguardo <strong>esistono molte teorie</strong>:
+		</p>
+		<ul>
+			<li>
+				<h3>La teoria del meteorite</h3>
+				<p>
+					Ogni giorno sulla Terra cadono  fino a 500 meteoriti  della grandezza di una pallina da tennis, eppure non ce ne
+					accorgiamo. Come ne cadono di piccoli tutti giorni, così ne cadono di immensi,  molto raramente.
+					Secondo alcuni studiosi, <strong>un meteorite di 10 chilometri</strong> si sarebbe schiantato sul suolo terrestre circa 65
+					milioni di anni fa nella penisola dello  Yucatan, in Messico, creando un cratere di ben  180 chilometri di 
+					diametro! Questo scontro terrificante avrebbe provocato una tremenda esplosione che avrebbe riempito l'aria di
+					polvere e sollevato impenetrabili nubi. La Terra, per via di queste, non avrebbe visto la luce del Sole per mesi
+					(alcuni ipotizzano per anni), e perciò il clima sarebbe cambiato repentinamente. Un gelido inverno avrebbe 
+					ricoperto il suolo terrestre uccidendo moltissime specie di piante, estinguendo i dinosauri erbivori (che non
+					trovarono più cibo per nutrirsi) e, di conseguenza, anche i carnivori, che se ne cibavano.
+				</p>
+			</li>
+			<li>
+				<h3>Le eruzioni vulcaniche</h3>
+				<p>
+					Recentemente, però, la teoria del meteorite è stata messa in discussione. Infatti, ricerche recenti affermano che
+					in realtà il meteorite si schiantò sul nostro pianeta ben 300 mila anni prima dell'estinzione dei dinosauri, dunque
+					non può essere ritenuto colpevole. Ecco allora che è stata elaborata una diversa teoria. Essa afferma che, sulla
+					Terra, vi fu un <strong>periodo di grandi sconvolgimenti causati da terremoti continui</strong>. A questi si aggiunsero imponenti
+					<strong>eruzioni vulcaniche</strong> che, liberando nell'atmosfera milioni di tonnellate di sostanze chimiche e ceneri,  resero 
+					l'aria tossica e oscurarono il sole, creando un terrificante effetto serra (in pratica, la nostra Terra non 
+					riusciva più a disperdere nello spazio l'eccesso di calore ricevuto dal Sole) che cambiò in modo radicale il
+					clima e le stagioni. L'insieme di questi fatti portò,  nel giro di poco tempo, all'estinzione dei dinosauri.
+				</p>
+			</li>
+			<li>
+				<h3>Un disastro...combinato?</h3>
+				<p>
+					Una recente teoria <strong>metterebbe in relazione la prima teoria (meteorite) con l'ipotesi delle eruzioni</strong>.
+					Alcuni ricercatori hanno infatti riscontrato importanti variazioni di 
+					temperatura avvenuta sulla Terra proprio durante il periodo in cui scomparvero i dinosauri. Ciò sarebbe 
+					riconducibile al fatto che i due disastri avvennero l'uno dopo l'altro, a circa 150.000 anni di distanza, 
+					provocando cambiamenti climatici così radicali da impedire ai lucertoloni di adattarsi.
+				</p>
+			</li>
+			<li>
+				<h3>Le malattie e la crisi esistenziale</h3>
+				<p>
+					Alcuni studiosi sostengono che un'<strong>epidemia virale o batterica</strong> avrebbe portato all'estinzione di questi animali 
+					giganteschi. Un gruppetto di altri studiosi sostiene invece che la colpa dell'estinzione più famosa della storia
+					sia colpa dei primi piccoli mammiferi, che cominciarono a cibarsi delle uova di dinosauro.
+				</p>
+			</li>
+			<li>
+				<h3>Il normale corso della natura</h3>
+				<p>
+					Quella chiamata <strong>"del normale corso della Natura"</strong> è l'ultima teoria che avanzata nel corso degli
+					anni. Essa afferma che l'estinzione dei dinosauri non è da ascrivere a nessun fattore in particolare ma al
+					semplice decorso della Natura. Pensiamo infatti ad animali come il mammut o la tigre dai denti a sciabola: 
+					si sono estinti in modo naturale, non a causa di alcun grande fattore ambientale o climatico.
+					Come loro, anche oggi molti animali rischiano di estinguersi: pensate alla balenottera azzurra, che è molto più
+					grande dei dinosauri e, di questo passo, fra poche centinaia di anni non ci sarà più! Pensiamo all'uccello dodo,
+					sterminato dagli uomini appena 300 anni fa. Come vediamo, ai normali cambiamenti climatici si sono aggiunti i
+					disastri causati dall'uomo, come la caccia e lo sfruttamento indiscriminato delle risorse naturali, vita compresa.
+				</p>
+			</li>	
+		</ul>
+	</div>
+</div>
+
+<div class="padding-12 parallax" style="background-image:url('img/volcano.jpg'); background-position:bottom"></div>
+
+<!-- /Content -->
+
+<?php include_once('footer.php') ?>
+
+<?php include_once('tothetop.php') ?>
+
+</div>
+
+</body>
+
+<!-- /Body -->
+
+</html>
