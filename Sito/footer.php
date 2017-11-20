991319db0f979dec6b4d58eee865cb028c7c67c1
Matte <matteo_rizzo96@hotmail.it>
2017-11-20 15:58:46 +0100

    Update footer.php

--- Sito/footer.php
+++ Sito/footer.php
@@ -1,6 +1,6 @@
-<footer class="w3-flat-green-sea padding-3">
-	<div class="w3-content w3-center">
-		<h3 class="w3-wide"> <strong> DINOSWAG <em> S.P.A </em> </strong> </h3>
+<footer class="green-sea padding-3">
+	<div class="content center">
+		<h3 class="wide"> <strong> DINOSWAG <em> S.P.A </em> </strong> </h3>
 		<a href="mailto:info@dinoSWAG.com"><p>info@dinoSWAG.com</p></a>
 	</div>
 </footer>
