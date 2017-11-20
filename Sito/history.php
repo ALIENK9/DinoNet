abe8cd91e2761d42d4ed5cb58a42db5fea17fd55
Matte <matteo_rizzo96@hotmail.it>
2017-11-20 22:06:24 +0100

    Update history.php

--- Sito/history.php
+++ Sito/history.php
@@ -7,9 +7,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="keywords" content="HTML, CSS, XML, JavaScript">
-	<link rel="stylesheet" href="css/w3.css">
 	<link rel="stylesheet" href="css/index.css">
-	<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-flat.css">
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 </head>
 
