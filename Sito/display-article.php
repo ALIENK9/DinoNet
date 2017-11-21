ca330eba5272e207d628fe6d6773f5825c3445c9
ALIENK9 <alezanga@hotmail.it>
2017-11-21 17:02:08 +0100

    display_article.php aggiornato + immagini nuove

--- Sito/display-article.php
+++ Sito/display-article.php
@@ -34,43 +34,38 @@
 <div class="padding-6 content">
 	<div id="daily-article" class="card">
 		<div class="padding-large green-sea">
-			<h1> Titolo dell'articolo </h1>
+			<h1> Estinzione: diverse teorie</h1>
 		</div>
-		<img src="img/dailyarticle-test.jpg" alt="immagine raffigurante resti di dinosauro">
+		<img id="petrified_wood" src="img/petrified_wood.jpg" alt="Sezione pietrificata di tronco fossile nel parco nazionale della Foresta Pietrificata in Arizona">
 		<div class="wrap-padding">
+			<p>
+				Dopo aver domiato la Terra per 170 milioni di anni, impedendo a tutte le altre forme (pesci, uccelli e soprattutto mammiferi) di affermarsi, i grandi rettili di colpo scomparvero. Sessantacinque milioni di anni fa, sulla Terra si produce un'immensa catastrofe che elimina tutti i dinosauri in ogni angolo del pianeta.<br>
+				Non solo: ma anche gran parte della vita si estingue, oltre che sulla terraferma, anche nei mari e nei cieli. Secondo alcune stime il 50-70% delle specie viventi scomparvero. In particolare si estinsero tutti gli animali di una certa taglia, ma anche moltissimi animali microscopici, e buona parte del plancton. Sulla scomparsa dei dinosauri sono state formulate più di 60 ipotesi. Troppe. Alcune del tutto bizzarre, ma altre più probabili.
+			</p>
+			<img id="meteor_crater" src="img/meteor_crater.jpg" alt="Immagine del Meteor Crater in Arizona">
 			<p>
 				Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
 				totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta
 				sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
 				consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
-				
-				<br><br>
-				
-				Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
-				totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta
-				sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
-				consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. 
-				
-				<br><br>
-				
-				Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
-				consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+			</p>
+			<p>
 				Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
 				totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta
 				sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
 				consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
-				
-				<br><br>
-				
-				Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
-				totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta
-				sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
-				consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. 
-				
-				<br><br>
-				
-				Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
-				consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+			</p>
+			<h2>Estinzione: e dopo?</h2>
+			<p>
+				L'era Cenozoica (che comprende l'attuale periodo Quaternario e il precedente Terziario) fece seguito all'estizione di massa della fine del Cretacico, che determinò la scomparsa dei dinosauri, degli pterosauri, dialcuni uccelli e mammiferi e di molte specie marine.
+			</p>
+			<p>
+				Nel corso del Terziario inferiore, i mammiferi e gli uccelli sopravissuti si diversificarono rapidamente, occupando le nicchie ecologiche
+				lasciate libere dai dinosauri. Nel Terziario superiore, durante le epoche del Miocene e Pliocene, la diffusione delle praterie condusse all'evoluzione di forme moderne di mammiferi erbivori.
+			</p>
+			<img id="carboniferous_forest" src="img/carboniferous_forest.jpg" alt="Uno scorcio di foresta del periodo Carbonifero">
+			<p>
+				A partire dal Quaternario (le cui epoche sono Pleistocene e Olocene), la vita animale e vegetale rassomigliava genericamente alle specie moderne, sebbene alcune, che si erano adattate alla sopravvivenza nel corso dei periodi glaciali, non siano sopravvissute fino ai giorni nostri. Alcune famiglie invece, sono giunte fino ai nostri giorni: per esempio le pteridofite, la grande famiglia delle felci, che fecero la loro apparizione durante il periodo Carbonifero, più di 300 milioni di anni fa.
 			</p>
 		</div>
 	</div>
