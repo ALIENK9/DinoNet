1c701fef5222e6ce58fae8aa09b9532e170ea589
Matte <matteo_rizzo96@hotmail.it>
2017-11-27 14:16:39 +0100

    Update display-specie.php

--- Sito/display-specie.php
+++ Sito/display-specie.php
@@ -34,14 +34,16 @@
 
 <!-- Dinosauro -->
 
-<div class="padding-6 content-large row-padding">
-	<div class="half wrap-padding">
-		<div class="card">
-			<div class="green-sea center wrap-padding">
-				<h1 class="title" xml:lang="en" lang="en">Coelopysis</h1>
-			</div>
+<div class="content-large padding-3">
+	<div class="card">
+		<div class="green-sea center wrap-padding">
+			<h1 class="title" xml:lang="en" lang="en">Coelopysis</h1>
+		</div>
+		<div class="wrap-padding w3-row-padding">
+		<div class="half wrap-padding">
 			<div class="white wrap-padding">
 				<h3>Caratteristiche</h3>
+				<br>
 				<ul>
 					<li><strong>Descritto da:</strong> Edward Drinker Cope, 1889</li>
 					<li><strong>Età:</strong> 208-200 milioni</li>
@@ -50,33 +52,40 @@
 					<li><strong>Peso:</strong> 46 kg</li>
 					<li><strong>Alimentazione:</strong> Rettili, pesci</li>
 				</ul>
-				<h3>Descrizione</h3>
-				<p>
-					Stando ai ritrovamenti effettuati fin’ora, uno dei primi dinosauri a popolare la regione sudoccidentale degli Stati Uniti verso la fine del Triassico (circa 215 milioni di anni fa) è il <span xml:lang="en" lang="en"><em>Coelophysis</em></span> (nome completo "<span xml:lang="en" lang="en"><em>Coelophysis Bauri</em></span>"). Il suo nome deriva dal greco e significa "forma cava" in riferimento alle sue ossa cave, in particolare a quelle del cranio, che sono dotate di piccole cavità per ridurre il peso dell’animale. Questa caratteristica lo distingue da altre specie di dinosauri più primitive come gli herrerasauridi vissuti  metà del Triassico. Una specializzazione molto particolare del <span xml:lang="en" lang="en"><em>Coelophysis</em></span> riguarda la coda, le cui vertebre erano stranamente dotate di strutture allungate che probabilmente limitavano molto il movimento verticale. Infatti si ritiene che servisse per controbilanciare il peso corporeo. Questa semirigidità può essere paragonata a quella dei successivi tetanuri, nei quali la coda è irrigidita dai tendini ossificati.
-				</p>
-				<br>
-				<p>
-					Lungo fino a tre metri e alto in media poco più di un metro, il <span xml:lang="en" lang="en"><em>Coelophysis</em></span> aveva una struttura leggera e agile, che gli consentiva di catturare lucertole, anfibi e insetti alati per cibarsene.
-					Per individuare e catturare la preda, si ritiene che il <span xml:lang="en" lang="en"><em>Coelophysis</em></span> si servisse dei grandi occhi e del collo lungo e serpentiforme, sufficientemente flessibile per girarsi con rapidità; la testa era lunga e stretta e le mascelle contenevano denti piccoli, aguzzi e aghiformi, dai bordi seghettati.
-					<br>
-					L’analisi dello scheletro suggerisce che il <span xml:lang="en" lang="en"><em>Coelophysis</em></span> si spostasse velocemente sulle due agili e magre zampe posteriori. Ciascuno degli arti anteriori, piuttosto corti (misuravano un terzo degli arti posteriori), terminava con quattro dita, tre delle quali funzionali e artigliate. Sono state rinvenute due forme di <span xml:lang="en" lang="en"><em>Coelophysis</em></span>: una più gracile, l'altra più robusta, e si ritiene che queste differenze rappresentano un caso di dimorfismo sessuale nella stessa specie.
-				</p>
-
-				<h3>Una curiosità</h3>
-				<p>
-					<strong> Lo sapevi che... </strong> nel 1947 una spedizione nel Nuovo Messico (<abbr xml:lang="en" lang="en" title="United States of America">USA</abbr>), capitanata da <span xml:lang="en" lang="en">Edwin Colbert</span>, fece una sensazionale scoperta. In una proprietà nota col nome di <span xml:lang="en" lang="en">Ghost Ranch</span> (“ranch fantasma”) vennero trovati centinaia di scheletri di <span xml:lang="en" lang="en"><em>Coelophysis</em></span>, che giacevano gli uni sugli altri. La scoperta era particolarmente eccitante, perché riguardava <span xml:lang="en" lang="en"><em>Coelophysis</em></span> di età e dimensioni differenti. Sembra che l'intero branco fosse andato incontro alla morte nel medesimo tempo. Ciò fa supporre che il decesso sia stato causato da un'improvvisa calamità, per esempio da una violenta inondazione che fece annegare gli animali, sommergendoli.
-				</p>
-				<br>
-				<p>
-					Nella cassa toracica di alcuni adulti si sono ritrovati resti di piccoli della stessa specie: all'inizio gli esperti pensarono che si trattasse di figli vicini alla nascita, ma non ancora venuti alla luce. Ma i dinosauri deponevano uova e non erano vivipari, perciò forse si trattava piuttosto degli avanzi di un ultimo pasto. Quegli scheletri, inoltre, erano davvero troppo grandi per poter essere contenuti in un uovo o per passare attraverso le ossa del bacino. Per quanto possa essere stata sorprendente, l'ipotesi che i <span xml:lang="en" lang="en"><em>Coelophysis</em></span> non si nutrissero solo di piccole lucertole ma anche dei membri giovanissimi della loro stessa specie sembrò decisamente realistica.<br>
-					Tuttavia, nonostante questa teoria sia stata più volte acclamata dalla maggior parte dei paleontologi, di recente è stata messa in discussione. L'accatastarsi delle ossa le une sulle altre, infatti, potrebbe aver portato <span xml:lang="en" lang="en">Edwin Colbert</span> e i suoi colleghi a ritenere di aver individuato resti di giovani esemplari nella cavità addominale degli adulti, quando essi potevano semplicemente essere stati "schiacciati" sotto un individuo più grande. In particolare, nel 2005 sono state rinvenute ulteriori prove che derivano dai contenuti dello stomaco fossilizzati in alcuni esemplari di <span xml:lang="en" lang="en"><em>Coelophysis</em></span>: i resti dei presunti "giovani" sarebbero da attribuire ad un’altro rettile, il crocodilomorfo Dromomeron.
-				</p>
 			</div>
 		</div>
-	</div>
-	<div class="half wrap-padding">
-		<div class="daily-dino card">
-			<img src="img/tiles/Coelophysis.jpg" alt="Ricostruzione di un Coelophysis">
+		<div class="half wrap-padding">
+			<div class="daily-dino">
+				<img src="img/tiles/Coelophysis.jpg" alt="Ricostruzione di un Coelophysis">
+			</div>
+		</div>
+		</div>
+		<div class="wrap-padding white">
+			<h3>Descrizione</h3>
+			<br>
+			<p>
+				Stando ai ritrovamenti effettuati fin’ora, uno dei primi dinosauri a popolare la regione sudoccidentale degli Stati Uniti verso la fine del Triassico (circa 215 milioni di anni fa) è il <span xml:lang="en" lang="en"><em>Coelophysis</em></span> (nome completo "<span xml:lang="en" lang="en"><em>Coelophysis Bauri</em></span>"). Il suo nome deriva dal greco e significa "forma cava" in riferimento alle sue ossa cave, in particolare a quelle del cranio, che sono dotate di piccole cavità per ridurre il peso dell’animale. Questa caratteristica lo distingue da altre specie di dinosauri più primitive come gli herrerasauridi vissuti  metà del Triassico. Una specializzazione molto particolare del <span xml:lang="en" lang="en"><em>Coelophysis</em></span> riguarda la coda, le cui vertebre erano stranamente dotate di strutture allungate che probabilmente limitavano molto il movimento verticale. Infatti si ritiene che servisse per controbilanciare il peso corporeo. Questa semirigidità può essere paragonata a quella dei successivi tetanuri, nei quali la coda è irrigidita dai tendini ossificati.
+			</p>
+			<br>
+			<p>
+				Lungo fino a tre metri e alto in media poco più di un metro, il <span xml:lang="en" lang="en"><em>Coelophysis</em></span> aveva una struttura leggera e agile, che gli consentiva di catturare lucertole, anfibi e insetti alati per cibarsene.
+				Per individuare e catturare la preda, si ritiene che il <span xml:lang="en" lang="en"><em>Coelophysis</em></span> si servisse dei grandi occhi e del collo lungo e serpentiforme, sufficientemente flessibile per girarsi con rapidità; la testa era lunga e stretta e le mascelle contenevano denti piccoli, aguzzi e aghiformi, dai bordi seghettati.
+				<br>
+				L’analisi dello scheletro suggerisce che il <span xml:lang="en" lang="en"><em>Coelophysis</em></span> si spostasse velocemente sulle due agili e magre zampe posteriori. Ciascuno degli arti anteriori, piuttosto corti (misuravano un terzo degli arti posteriori), terminava con quattro dita, tre delle quali funzionali e artigliate. Sono state rinvenute due forme di <span xml:lang="en" lang="en"><em>Coelophysis</em></span>: una più gracile, l'altra più robusta, e si ritiene che queste differenze rappresentano un caso di dimorfismo sessuale nella stessa specie.
+			</p>
+			
+			<br>
+			
+			<h3>Una curiosità</h3>
+			<br>
+			<p>
+				<strong> Lo sapevi che... </strong> nel 1947 una spedizione nel Nuovo Messico (<abbr xml:lang="en" lang="en" title="United States of America">USA</abbr>), capitanata da <span xml:lang="en" lang="en">Edwin Colbert</span>, fece una sensazionale scoperta. In una proprietà nota col nome di <span xml:lang="en" lang="en">Ghost Ranch</span> (“ranch fantasma”) vennero trovati centinaia di scheletri di <span xml:lang="en" lang="en"><em>Coelophysis</em></span>, che giacevano gli uni sugli altri. La scoperta era particolarmente eccitante, perché riguardava <span xml:lang="en" lang="en"><em>Coelophysis</em></span> di età e dimensioni differenti. Sembra che l'intero branco fosse andato incontro alla morte nel medesimo tempo. Ciò fa supporre che il decesso sia stato causato da un'improvvisa calamità, per esempio da una violenta inondazione che fece annegare gli animali, sommergendoli.
+			</p>
+			<br>
+			<p>
+				Nella cassa toracica di alcuni adulti si sono ritrovati resti di piccoli della stessa specie: all'inizio gli esperti pensarono che si trattasse di figli vicini alla nascita, ma non ancora venuti alla luce. Ma i dinosauri deponevano uova e non erano vivipari, perciò forse si trattava piuttosto degli avanzi di un ultimo pasto. Quegli scheletri, inoltre, erano davvero troppo grandi per poter essere contenuti in un uovo o per passare attraverso le ossa del bacino. Per quanto possa essere stata sorprendente, l'ipotesi che i <span xml:lang="en" lang="en"><em>Coelophysis</em></span> non si nutrissero solo di piccole lucertole ma anche dei membri giovanissimi della loro stessa specie sembrò decisamente realistica.<br>
+				Tuttavia, nonostante questa teoria sia stata più volte acclamata dalla maggior parte dei paleontologi, di recente è stata messa in discussione. L'accatastarsi delle ossa le une sulle altre, infatti, potrebbe aver portato <span xml:lang="en" lang="en">Edwin Colbert</span> e i suoi colleghi a ritenere di aver individuato resti di giovani esemplari nella cavità addominale degli adulti, quando essi potevano semplicemente essere stati "schiacciati" sotto un individuo più grande. In particolare, nel 2005 sono state rinvenute ulteriori prove che derivano dai contenuti dello stomaco fossilizzati in alcuni esemplari di <span xml:lang="en" lang="en"><em>Coelophysis</em></span>: i resti dei presunti "giovani" sarebbero da attribuire ad un’altro rettile, il crocodilomorfo Dromomeron.
+			</p>
 		</div>
 	</div>
 </div>
