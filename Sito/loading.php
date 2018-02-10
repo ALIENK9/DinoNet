112c9aac49be39f2ef0af630511eeb85913901e7
Matteo Rizzo <matteo96rizzo@gmail.com>
2018-02-10 13:06:08 +0100

    Aggiunte animazioni a relazioni, piccoli fix

--- Sito/loading.php
+++ Sito/loading.php
@@ -7,21 +7,4 @@
     </div>
 </div>
 
-<!-- Loading - script -->
-<script>
-    document.getElementById("loading").classList.remove("require-js"); //rende visibile l'animazione di loading
-
-	$(document).ready(
-		function() {
-			$(window).load(
-				function() {
-					$(".loading").fadeOut("slow");
-				}
-			);
-		}
-	);
-</script>
-
-<!-- Loading - script -->
-
 <!-- /Loading -->
